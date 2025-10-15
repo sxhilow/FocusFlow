8cf1cfe36eedb56b05ab9442e03e0fe169bbac06
Sahil Jada <jadasahil786@gmail.com>
2025-10-15 11:24:56 +0200

    chore: project structure setup

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # FocusFlow
+
+FocusFlow allows you to log in your daily taks and be productive in a gamefied way.
---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <p className='text-4xl text-blue-500'>Focus Flow</p>
+      </div>
+    </>
+  )
+}
+
+export default App
